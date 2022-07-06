b9f8916e5b8b89c5838a32e6f68b4fb3dfebcc99
rlaeodnr1011 <skfvkfl1011@gmail.com>
2022-07-06 11:03:30 +0900

    fix: 언어를 한국어로 변경

--- src/config/settings/settings.py
+++ src/config/settings/settings.py
@@ -104,8 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-# LANGUAGE_CODE = 'ko-kr'
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
